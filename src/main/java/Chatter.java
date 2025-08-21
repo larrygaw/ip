510931b2980b718af9736ecdf9c6a40bc1caf8a7
larrygaw <larrygaw2093@gmail.com>
2025-08-21 17:52:41 +0800

    Rename class and change message

---
+++ src/main/java/Chatter.java
@@ -0,0 +1,8 @@
+public class Chatter {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________\n";
+        String defaultMessage = "Hello! I'm Chatter\nWhat can I do for you?\n";
+        String exitMessage = "Bye. Hope to see you again soon!\n";
+        System.out.println(line + defaultMessage + line + exitMessage + line);
+    }
+}
