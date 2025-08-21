ab0ac1d336c82842e72a4864e973038c4e70bdda
larrygaw <larrygaw2093@gmail.com>
2025-08-21 22:40:41 +0800

    Add ability to mark/unmark task as done

--- src/main/java/Chatter.java
+++ src/main/java/Chatter.java
@@ -5,9 +5,12 @@ public class Chatter {
         String line = "   ____________________________________________________________";
         String defaultMessage = "   Hello! I'm Chatter\n   What can I do for you?";
         String exitMessage = "   Bye. Hope to see you again soon!";
+        String listMessage = "   Here are the tasks in your list:";
+        String marking = "   Nice! I've marked this task as done:";
+        String unmarking = "   OK, I've marked this task as not done yet:";
 
         //storage for task
-        String[] taskList = new String[100];
+        Task[] taskList = new Task[100];
         int taskCounter = 0;
 
         System.out.println(line);
@@ -25,20 +28,56 @@ public class Chatter {
                 System.out.println(exitMessage);
                 System.out.println(line);
                 break;
-            }
-            else if (input.equals("list")) {
+            } else if (input.equals("list")) {
                 System.out.println(line);
+                System.out.println(listMessage);
                 //print every task in taskList
                 for(int i = 0; i < taskCounter; i++) {
-                    System.out.println("   " + (i + 1) + ". " + taskList[i]);
+                    System.out.println("   " + (i + 1) + "." + taskList[i]);
                 }
                 System.out.println(line);
-            }
-            else {
+            } else if (input.startsWith("mark ") || input.startsWith("unmark ")){
+                String[] parts = input.split(" ");
+                if (parts.length == 2) {
+                    try {
+                        int index = Integer.parseInt(parts[1]) - 1;
+                        Task t = taskList[index];
+                        if (parts[0].equals("mark")) {
+                            t.markAsDone();
+                            System.out.println(line);
+                            System.out.println(marking);
+                            System.out.println("     " + t);
+                            System.out.println(line);
+                        } else {
+                            t.unmark();
+                            System.out.println(line);
+                            System.out.println(unmarking);
+                            System.out.println("     " + t);
+                            System.out.println(line);
+                        }
+                    }
+                    catch (NumberFormatException e) {
+                        System.out.println(line);
+                        System.out.println("   added: " + input);
+                        System.out.println(line);
+                        taskList[taskCounter] = new Task(input);
+                        taskCounter += 1;
+                    }
+                    catch (ArrayIndexOutOfBoundsException | NullPointerException e) {
+                        System.out.println("No such task!");
+                    }
+                } else {
+                    System.out.println(line);
+                    System.out.println("   added: " + input);
+                    System.out.println(line);
+                    taskList[taskCounter] = new Task(input);
+                    taskCounter += 1;
+                }
+            } else {
                 System.out.println(line);
                 System.out.println("   added: " + input);
                 System.out.println(line);
-                taskList[taskCounter] = input;
+                taskList[taskCounter] = new Task(input);
                 taskCounter += 1;
             }
         }
