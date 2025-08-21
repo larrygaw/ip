853d41f5a55052787c97d3b267a0d28aa591176f
larrygaw <larrygaw2093@gmail.com>
2025-08-21 18:15:20 +0800

    Add support for echo

--- src/main/java/Chatter.java
+++ src/main/java/Chatter.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
+
 public class Chatter {
     public static void main(String[] args) {
-        String line = "____________________________________________________________\n";
-        String defaultMessage = "Hello! I'm Chatter\nWhat can I do for you?\n";
-        String exitMessage = "Bye. Hope to see you again soon!\n";
-        System.out.println(line + defaultMessage + line + exitMessage + line);
+        String line = "   ____________________________________________________________";
+        String defaultMessage = "   Hello! I'm Chatter\n   What can I do for you?";
+        String exitMessage = "   Bye. Hope to see you again soon!";
+
+        System.out.println(line);
+        System.out.println(defaultMessage);
+        System.out.println(line);
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            //to read what user types on keyboard
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(line);
+                System.out.println(exitMessage);
+                System.out.println(line);
+                break;
+            }
+            else {
+                System.out.println(line);
+                System.out.println("   " + input);
+                System.out.println(line);
+            }
+        }
+
+        sc.close();
     }
 }
