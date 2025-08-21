a5de73f6454737de0139e5289195b8abbf8f6839
larrygaw <larrygaw2093@gmail.com>
2025-08-21 20:43:46 +0800

    Add support for list

--- src/main/java/Chatter.java
+++ src/main/java/Chatter.java
@@ -6,6 +6,10 @@ public class Chatter {
         String defaultMessage = "   Hello! I'm Chatter\n   What can I do for you?";
         String exitMessage = "   Bye. Hope to see you again soon!";
 
+        //storage for task
+        String[] taskList = new String[100];
+        int taskCounter = 0;
+
         System.out.println(line);
         System.out.println(defaultMessage);
         System.out.println(line);
@@ -15,19 +19,29 @@ public class Chatter {
         while(true) {
             //to read what user types on keyboard
             String input = sc.nextLine();
+
             if (input.equals("bye")) {
                 System.out.println(line);
                 System.out.println(exitMessage);
                 System.out.println(line);
                 break;
             }
-            else {
+            else if (input.equals("list")) {
                 System.out.println(line);
-                System.out.println("   " + input);
+                //print every task in taskList
+                for(int i = 0; i < taskCounter; i++) {
+                    System.out.println("   " + (i + 1) + ". " + taskList[i]);
+                }
                 System.out.println(line);
             }
+            else {
+                System.out.println(line);
+                System.out.println("   added: " + input);
+                System.out.println(line);
+                taskList[taskCounter] = input;
+                taskCounter += 1;
+            }
         }
-
         sc.close();
     }
 }
