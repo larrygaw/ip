b81d4d1a425f3fce8912e2690fa2ada714d623ab
larrygaw <larrygaw2093@gmail.com>
2025-08-22 01:08:50 +0800

    Add support for tracking three types of tasks

--- src/main/java/Chatter.java
+++ src/main/java/Chatter.java
@@ -8,6 +8,7 @@ public class Chatter {
         String listMessage = "   Here are the tasks in your list:";
         String marking = "   Nice! I've marked this task as done:";
         String unmarking = "   OK, I've marked this task as not done yet:";
+        String addingTask = "   Got it. I've added this task:";
 
         //storage for task
         Task[] taskList = new Task[100];
@@ -74,11 +75,44 @@ public class Chatter {
                     taskCounter += 1;
                 }
             } else {
-                System.out.println(line);
-                System.out.println("   added: " + input);
-                System.out.println(line);
-                taskList[taskCounter] = new Task(input);
-                taskCounter += 1;
+                if (input.startsWith("todo")) {
+                    String[] parts = input.split(" ", 2);
+                    ToDos t = new ToDos(parts[1]);
+                    taskList[taskCounter] = t;
+                    taskCounter += 1;
+                    System.out.println(line);
+                    System.out.println(addingTask);
+                    System.out.println("     " + t);
+                    System.out.println("   Now you have " + taskCounter + " tasks in the list.");
+                    System.out.println(line);
+                } else if (input.startsWith("deadline")) {
+                    String[] parts = input.split(" ", 2);
+                    String[] details = parts[1].split(" /by ", 2);
+                    Deadline t = new Deadline(details[0], details[1]);
+                    taskList[taskCounter] = t;
+                    taskCounter += 1;
+                    System.out.println(line);
+                    System.out.println(addingTask);
+                    System.out.println("     " + t);
+                    System.out.println("   Now you have " + taskCounter + " tasks in the list.");
+                    System.out.println(line);
+                } else if (input.startsWith("event")) {
+                    String[] parts = input.split(" ", 2);
+                    String[] getDetails = parts[1].split(" /from ", 2);
+                    String[] getTiming = getDetails[1].split(" /to ");
+                    Events t = new Events(getDetails[0], getTiming[0], getTiming[1]);
+                    taskList[taskCounter] = t;
+                    taskCounter += 1;
+                    System.out.println(line);
+                    System.out.println(addingTask);
+                    System.out.println("     " + t);
+                    System.out.println("   Now you have " + taskCounter + " tasks in the list.");
+                    System.out.println(line);
+                } else {
+                    System.out.println(line);
+                    System.out.println("No such task!");
+                    System.out.println(line);
+                }
             }
         }
         sc.close();
