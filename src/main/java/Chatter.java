41c5548a7cbe707cb8af4ba141f6011156a4cf47
larrygaw <larrygaw2093@gmail.com>
2025-08-22 14:55:03 +0800

    Add support to handle errors

--- src/main/java/Chatter.java
+++ src/main/java/Chatter.java
@@ -20,63 +20,29 @@ public class Chatter {
 
         Scanner sc = new Scanner(System.in);
 
-        while(true) {
-            //to read what user types on keyboard
-            String input = sc.nextLine();
+        while (true) {
+            try {
+                //to read what user types on keyboard
+                String input = sc.nextLine();
 
-            if (input.equals("bye")) {
-                System.out.println(line);
-                System.out.println(exitMessage);
-                System.out.println(line);
-                break;
-            } else if (input.equals("list")) {
-                System.out.println(line);
-                System.out.println(listMessage);
-                //print every task in taskList
-                for(int i = 0; i < taskCounter; i++) {
-                    System.out.println("   " + (i + 1) + "." + taskList[i]);
-                }
-                System.out.println(line);
-            } else if (input.startsWith("mark ") || input.startsWith("unmark ")){
-                String[] parts = input.split(" ");
-                if (parts.length == 2) {
-                    try {
-                        int index = Integer.parseInt(parts[1]) - 1;
-                        Task t = taskList[index];
-                        if (parts[0].equals("mark")) {
-                            t.markAsDone();
-                            System.out.println(line);
-                            System.out.println(marking);
-                            System.out.println("     " + t);
-                            System.out.println(line);
-                        } else {
-                            t.unmark();
-                            System.out.println(line);
-                            System.out.println(unmarking);
-                            System.out.println("     " + t);
-                            System.out.println(line);
-                        }
-                    }
-                    catch (NumberFormatException e) {
-                        System.out.println(line);
-                        System.out.println("   added: " + input);
-                        System.out.println(line);
-                        taskList[taskCounter] = new Task(input);
-                        taskCounter += 1;
-                    }
-                    catch (ArrayIndexOutOfBoundsException | NullPointerException e) {
-                        System.out.println("No such task!");
-                    }
-                } else {
+                if (input.equals("bye")) {
                     System.out.println(line);
-                    System.out.println("   added: " + input);
+                    System.out.println(exitMessage);
                     System.out.println(line);
-                    taskList[taskCounter] = new Task(input);
-                    taskCounter += 1;
-                }
-            } else {
-                if (input.startsWith("todo")) {
+                    break;
+                } else if (input.equals("list")) {
+                    System.out.println(line);
+                    System.out.println(listMessage);
+                    //print every task in taskList
+                    for (int i = 0; i < taskCounter; i++) {
+                        System.out.println("   " + (i + 1) + "." + taskList[i]);
+                    }
+                    System.out.println(line);
+                } else if (input.startsWith("todo")) {
                     String[] parts = input.split(" ", 2);
+                    if (parts.length < 2 || parts[1].isBlank()) {
+                        throw new ChatterException("todoTask must have a description!");
+                    }
                     ToDos t = new ToDos(parts[1]);
                     taskList[taskCounter] = t;
                     taskCounter += 1;
@@ -87,6 +53,9 @@ public class Chatter {
                     System.out.println(line);
                 } else if (input.startsWith("deadline")) {
                     String[] parts = input.split(" ", 2);
+                    if (parts.length < 2 || !parts[1].contains("/by")) {
+                        throw new ChatterException("deadlineTask must have description and /by!");
+                    }
                     String[] details = parts[1].split(" /by ", 2);
                     Deadline t = new Deadline(details[0], details[1]);
                     taskList[taskCounter] = t;
@@ -98,6 +67,9 @@ public class Chatter {
                     System.out.println(line);
                 } else if (input.startsWith("event")) {
                     String[] parts = input.split(" ", 2);
+                    if (parts.length < 2 || !parts[1].contains("/from") || !parts[1].contains("/to")) {
+                        throw new ChatterException("eventTask must have description, /from and /to!");
+                    }
                     String[] getDetails = parts[1].split(" /from ", 2);
                     String[] getTiming = getDetails[1].split(" /to ");
                     Events t = new Events(getDetails[0], getTiming[0], getTiming[1]);
@@ -108,11 +80,41 @@ public class Chatter {
                     System.out.println("     " + t);
                     System.out.println("   Now you have " + taskCounter + " tasks in the list.");
                     System.out.println(line);
+                } else if (input.startsWith("mark") || input.startsWith("unmark")) {
+                    String[] parts = input.split(" ");
+                    if (parts.length < 2) {
+                        throw new ChatterException("Provide index!");
+                    }
+                    int index = Integer.parseInt(parts[1]) - 1;
+                    Task t = taskList[index];
+                    if (parts[0].equals("mark")) {
+                        t.markAsDone();
+                        System.out.println(line);
+                        System.out.println(marking);
+                        System.out.println("     " + t);
+                        System.out.println(line);
+                    } else {
+                        t.unmark();
+                        System.out.println(line);
+                        System.out.println(unmarking);
+                        System.out.println("     " + t);
+                        System.out.println(line);
+                    }
                 } else {
-                    System.out.println(line);
-                    System.out.println("No such task!");
-                    System.out.println(line);
+                    throw new ChatterException("SORRY! I am not qualified to do this!");
                 }
+            } catch (ChatterException e){
+                System.out.println(line);
+                System.out.println("   " + e.getMessage());
+                System.out.println(line);
+            } catch (NumberFormatException e){
+                System.out.println(line);
+                System.out.println("   Task number must be an integer!");
+                System.out.println(line);
+            } catch (ArrayIndexOutOfBoundsException | NullPointerException e){
+                System.out.println(line);
+                System.out.println("   You don't have that many tasks!");
+                System.out.println(line);
             }
         }
         sc.close();
